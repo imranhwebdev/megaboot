1e9331b755bcb4e36c992b3f2cb2042714e1acb9
imran <alimrancv870@gmail.com>
2023-10-13 12:11:25 +0600

    htmldone

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,18 @@
             }
 
         });
-
-
+       
     });
 })();
+
+let copyText = document.querySelector(".copy__text");
+copyText.querySelector("button").addEventListener("click", function(){
+    let input = copyText.querySelector("input.text");
+    input.select();
+    document.execCommand('copy');
+    copyText.classList.add('active');
+    window.getSelection().removeAllRanges();
+    setTimeout(function(){
+        copyText.classList.remove("active");
+    }, 2500)
+})
